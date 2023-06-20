6801d314d9d93b2488fde550530834ebba81822a
Eduardo Orsi <duduayr@gmail.com>
2023-06-20 11:38:03 -0300

    adding categorys crud

--- gestao_financas/views.py
+++ gestao_financas/views.py
@@ -18,11 +18,11 @@ def profile(request: HttpRequest):
 def wallet(request: HttpRequest, pk = None):
     user_id = request.user.id
     if pk:
-        wallet = Wallet.objects.get(id=pk)
+        wallet_model = Wallet.objects.get(id=pk)
         if not request.method == "POST":
 
             context = {
-                "wallet": wallet
+                "wallet": wallet_model
             }
             return render(request, 'add_wallet.html', context)
         else:
@@ -33,17 +33,11 @@ def wallet(request: HttpRequest, pk = None):
                 messages.error(request, "Nome da certeira é necessário")
                 return render(request, "add_wallet.html", {})
             
-            wallet.name = name
-            wallet.description = description
-            wallet.save()
-
-            user_wallets = Wallet.objects.filter(user_id=user_id)
-            context = {
-                "wallets": user_wallets
-            }
-            return render(request, 'wallet.html', context)
+            wallet_model.name = name
+            wallet_model.description = description
+            wallet_model.save()
+            return wallet(request)
     
-
     user_wallets = Wallet.objects.filter(user_id=user_id)
     context = {
         "wallets": user_wallets
@@ -105,8 +99,54 @@ def add_transaction(request: HttpRequest):
         return render(request, 'add_transaction.html', {})
 
 @login_required()
-def categorys(request: HttpRequest):
+def categorys(request: HttpRequest, pk = None):
     user_id = request.user.id
+
+    if pk:
+        revenue_category = None
+        expense_category = None
+        try:
+            revenue_category = RevenueCategory.objects.get(id=pk)
+        except:
+            expense_category = ExpenseCategory.objects.get(id=pk)
+
+        if not request.method == "POST":
+            context = {}
+            if revenue_category:
+                context["category"] = revenue_category
+            else:
+                context["category"] = expense_category
+            
+            return render(request, "add_category.html", context)
+        
+        else:
+            name = request.POST.get("name", None)
+            description = request.POST.get("description", None)
+            essential = request.POST.get("essential", None)
+            is_expense = request.POST.get("expense", None)
+            is_revenue = request.POST.get("revenue", None)
+
+            if not name:
+                messages.error(request, "O nome da categoria é necessário")
+                return redirect("categorys")
+            
+            if not essential:
+                messages.error(request, "É essencial informar se é uma categoria essencial ou não")
+                return redirect("categorys")
+        
+            essential = bool(int(essential))
+
+            if revenue_category:
+                revenue_category.name = name
+                revenue_category.description = description
+                revenue_category.essential = essential
+                revenue_category.save()
+            else:
+                expense_category.name = name
+                expense_category.description = description
+                expense_category.essential = essential
+                expense_category.save()
+            return categorys(request)
 
     expense_categorys = ExpenseCategory.objects.filter(user_id=user_id)
     revenue_categorys = RevenueCategory.objects.filter(user_id=user_id)
@@ -160,8 +200,26 @@ def add_categorys(request: HttpRequest):
         return render(request, 'add_category.html', {})
     
 @login_required()
-def delete_categorys(request: HttpRequest):
-    pass
+def delete_categorys(request: HttpRequest, pk = None):
+
+    if not pk:
+        messages.error(request, "O ID da categoria é necessário")
+        return redirect("categorys")
+    
+    revenue_category = None
+    expense_category = None
+    
+    try:
+        revenue_category = RevenueCategory.objects.get(id=pk)
+    except:
+        expense_category = ExpenseCategory.objects.get(id=pk)
+    
+    if revenue_category:
+        revenue_category.delete()
+    else:
+        expense_category.delete()
+
+    return redirect("categorys")
 
 @login_required()
 def reports(request: HttpRequest):
